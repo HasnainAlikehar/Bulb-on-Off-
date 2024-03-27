554bd43959cbeb2b74d2813dd6934a1b584aa23e
HasnainAlikehar <hasnainalimpb@gmail.com>
2024-03-27 23:38:16 +0500

    add files

---
+++ app.js
@@ -0,0 +1,15 @@
+// Events classs
+
+// || Class Task
+
+const img = document.querySelector("#img")
+
+img.addEventListener('mouseover' , function() {
+    img.src = 'https://t3.ftcdn.net/jpg/02/80/82/08/240_F_280820881_L6GcEpWhFd32op8Guc2DBPvnMJQfix6u.jpg'
+})
+
+img.addEventListener('mouseout' , function() {
+    img.src = 'https://t4.ftcdn.net/jpg/00/99/63/85/240_F_99638575_jFLMbP4rFcWMrtL3PyupKOKP0d2iD7cm.jpg'
+})
+
+
